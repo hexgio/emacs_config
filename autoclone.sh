b13d67a2448e9de85e236472ce0080cf7ca31b1a
hexgio <hexgio@outlook.com>
2023-07-23 21:56:11 +0800

    config

--- autoclone.sh
+++ autoclone.sh
@@ -2,3 +2,4 @@
 # bash的位置根据终端环境的不同会有变化，可以根据自己终端环境进行修改
 
 git clone git@github.com:hexgio/emacs_config.git | xargs chmod -R +x emacs_config
+# 将 git@github.com:hexgio/emacs_config.git 仓库的文本 clone 下来
--- autopush.sh
+++ autopush.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash 
 # bash的位置根据终端环境的不同会有变化，可以根据自己终端环境进行修改
 
-find ./ -name "*~" -exec rm '{}' \;
+find ./ -name "*~" -exec rm '{}' \; 
+# 在上传本地文件之前将 emacs 编辑文件过程中产生的 *~ 文件给删除掉
 git add .; git commit -m "config"; git push -u origin main
+# 添加文件，提交修改，上传到远程仓库分支
