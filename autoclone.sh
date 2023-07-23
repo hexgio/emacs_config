9b9ddf64dd394c1978a650ec665cc0e19553478b
hexgio <hexgio@outlook.com>
2023-07-23 08:03:58 +0800

    config

--- autoclone.sh
+++ autoclone.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 find ./ -name "*~" -exec rm '{}' \;
-git clone git@github.com:hexgio/emacs_config.git
+git clone git@github.com:hexgio/emacs_config.git | xargs chmod -R +x emacs_config
