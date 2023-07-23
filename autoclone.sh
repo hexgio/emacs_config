38951271baf672bba6229168e7c07cc427947edd
hexgio <hexgio@outlook.com>
2023-07-23 21:48:36 +0800

    config

--- autoclone.sh
+++ autoclone.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/bash 
+# bash的位置根据终端环境的不同会有变化，可以根据自己终端环境进行修改
 
-find ./ -name "*~" -exec rm '{}' \;
 git clone git@github.com:hexgio/emacs_config.git | xargs chmod -R +x emacs_config
--- autopush.sh
+++ autopush.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/bash
+#!/usr/bin/bash 
+# bash的位置根据终端环境的不同会有变化，可以根据自己终端环境进行修改
 
 find ./ -name "*~" -exec rm '{}' \;
 git add .; git commit -m "config"; git push -u origin main
