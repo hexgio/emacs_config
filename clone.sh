1ca2d72858a22eb5529cb837daf3a86979b43b7f
hexgio <hexgio@outlook.com>
2023-08-25 08:51:00 +0800

    config

--- clone.sh
+++ clone.sh
@@ -1,2 +1,2 @@
-git clone git@github.com:hexgio/emacs_config.git | xargs chmod -R +x emacs_config
+git clone https://github.com/hexgio/emacs_config.git | xargs chmod -R +x emacs_config
 # 将 git@github.com:hexgio/emacs_config.git 仓库的文本 clone 下来
