fc277fbd53e671a6fe8502f32590dca8ff849973
hexgio <hexgio@outlook.com>
2023-08-25 09:17:00 +0800

    config

--- clone.sh
+++ clone.sh
@@ -1,2 +1,3 @@
-git clone https://github.com/hexgio/emacs_config.git | xargs chmod -R +x emacs_config
+# git clone https://github.com/hexgio/emacs_config.git | xargs chmod -R +x emacs_config
+git clone https://hub.nuaa.cf/hexgio/emacs_config.git | xargs chmod -R +x emacs_config # 有时候官网的克隆不下来可以用镜像网站进行克隆
 # 将 git@github.com:hexgio/emacs_config.git 仓库的文本 clone 下来
